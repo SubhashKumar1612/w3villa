606b62bbe3c64b99318a22cb4f871a1475b15340
SubhashKumar1612 <subhashkumarr1612@gmail.com>
2025-01-12 12:41:39 +0530

    add cloud database that is mongodb atlas

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,7 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/Auth")
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://subhashkumar:NvRSZp6p79gJ3fy@cluster0.tzilleb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" ||"mongodb://127.0.0.1:27017/Auth";
+
+mongoose.connect(mongoURI)
 .then(()=>{
     console.log("connection is successful")
 }).catch((err)=>{
